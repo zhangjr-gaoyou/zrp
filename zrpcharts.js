1cb9fb5bb5cecc3852367e200849289724b35f20
zhangjr-gaoyou <zhangjr_gaoyou@163.com>
2024-01-26 16:10:54 +0800

    Update zrpcharts.js

--- zrpcharts.js
+++ zrpcharts.js
@@ -35,7 +35,6 @@ if (window.ZrpChartScriptHasRun) {
 
         let sel = "#" + edom.id;
         console.log(sel, $(sel).position().left, $(sel).position().top);
-        console.log("[[This.name]],[[This.left]],[[This.top]]");
         /*
         if ($(sel).position().left == [[This.left]] && $(sel).position().top == [[This.top]]) {
           console.log("This element no:", i);
@@ -54,119 +53,234 @@ if (window.ZrpChartScriptHasRun) {
 
         switch (arrnames[1]) {
           case 'bar3':
-            /* get repeater data */
-            let repeater;
-            $axure(function (obj) {
-              return obj.type == 'repeater';
-            }).each(function (obj, id) {
-              if (id == curr_repeater_id) {
-                repeater = obj;
-              }
-            });
-            console.log(repeater.dataProps);
-            console.log(repeater.data);
-            console.log(repeater);
-
-
-            /* get config data */
-            let configer;
-            $axure(function (obj) {
-              return obj.type == 'repeater';
-            }).each(function (obj, id) {
-              if (id == curr_config_id) {
-                configer = obj;
-              }
-            });
-            console.log(configer.dataProps);
-            console.log(configer.data);
-
-
-
-            let yaxisdata = [];
-            let seriesdata = [];
-
-            for (let tmpi = 0; tmpi < repeater.data.length; tmpi++) {
-
-              yaxisdata.push(repeater.data[tmpi].name.text);
-
-              seriesdata.push(repeater.data[tmpi].value.text);
-
-            }
-
-
-
-            console.log("dataset=", yaxisdata, seriesdata);
-            let myChart = echarts.init(curr_chart_dom, '[[ZRP_DISPLAYMODE]]');
-
-            let option = {
-
-              tooltip: {
-                trigger: 'axis',
-                axisPointer: {
-                  type: 'shadow'
+            {
+              /* get repeater data */
+              let repeater;
+              $axure(function (obj) {
+                return obj.type == 'repeater';
+              }).each(function (obj, id) {
+                if (id == curr_repeater_id) {
+                  repeater = obj;
                 }
-              },
+              });
+              console.log(repeater.dataProps);
+              console.log(repeater.data);
+              console.log(repeater);
 
-              grid: {
-                left: '3%',
-                right: '8%',
-                bottom: '3%',
-                top: '3%',
-                containLabel: true
-              },
-              xAxis: {
-                type: 'value'
-              },
-              yAxis: {
-                type: 'category',
-                /*
-                data: ['数据一', '数据二', '数据三', '数据四', '数据五', '数据六', '数据七']
-                */
-                data: yaxisdata
-              },
-              series: [
-                {
-                  name: configer.data[0].dimname.text,
-                  type: 'bar',
-                  showBackground: true,
-                  backgroundStyle: {
-                    color: configer.data[0].bgcolor.text,
-                    borderRadius: 2
-                  },
 
-                  color:
+              /* get config data */
+              let configer;
+              $axure(function (obj) {
+                return obj.type == 'repeater';
+              }).each(function (obj, id) {
+                if (id == curr_config_id) {
+                  configer = obj;
+                }
+              });
+              console.log(configer.dataProps);
+              console.log(configer.data);
+
+
+
+              let yaxisdata = [];
+              let seriesdata = [];
+
+              for (let tmpi = 0; tmpi < repeater.data.length; tmpi++) {
+
+                yaxisdata.push(repeater.data[tmpi].name.text);
+
+                seriesdata.push(repeater.data[tmpi].value.text);
+
+              }
+
+
+
+              console.log("dataset=", yaxisdata, seriesdata);
+              let myChart = echarts.init(curr_chart_dom, '[[ZRP_DISPLAYMODE]]');
+
+              let option = {
+
+                tooltip: {
+                  trigger: 'axis',
+                  axisPointer: {
+                    type: 'shadow'
+                  }
+                },
+
+                grid: {
+                  left: '3%',
+                  right: '8%',
+                  bottom: '3%',
+                  top: '3%',
+                  containLabel: true
+                },
+                xAxis: {
+                  type: 'value'
+                },
+                yAxis: {
+                  type: 'category',
+                  /*
+                  data: ['数据一', '数据二', '数据三', '数据四', '数据五', '数据六', '数据七']
+                  */
+                  data: yaxisdata
+                },
+                series: [
                   {
-                    type: 'linear',
-                    x: 0,
-                    y: 0,
-                    x2: 1,
-                    y2: 1,
-                    colorStops: [{
-                      offset: 0, color: configer.data[0].bar0color.text
-                    }, {
-                      offset: 1, color: configer.data[0].bar1color.text
-                    }],
-                    global: false
-                  },
+                    name: configer.data[0].dimname.text,
+                    type: 'bar',
+                    showBackground: true,
+                    backgroundStyle: {
+                      color: configer.data[0].bgcolor.text,
+                      borderRadius: 2
+                    },
 
-                  stack: 'total',
-                  label: {
-                    show: true,
-                    fontSize: 16,
-                    backgroundColor: 'transparent',
-                    color: 'inherit',
-                    position: 'right'
-                  },
+                    color:
+                    {
+                      type: 'linear',
+                      x: 0,
+                      y: 0,
+                      x2: 1,
+                      y2: 1,
+                      colorStops: [{
+                        offset: 0, color: configer.data[0].bar0color.text
+                      }, {
+                        offset: 1, color: configer.data[0].bar1color.text
+                      }],
+                      global: false
+                    },
 
-                  data: seriesdata
+                    stack: 'total',
+                    label: {
+                      show: true,
+                      fontSize: 16,
+                      backgroundColor: 'transparent',
+                      color: 'inherit',
+                      position: 'right'
+                    },
+
+                    data: seriesdata
+                  }
+
+
+                ]
+              };
+              /* option end */
+              if (option && typeof option === "object") {
+                myChart.setOption(option, true);
+              }
+            }
+            break;
+          case 'line4':
+            {/* get repeater data */
+              let repeater;
+              $axure(function (obj) {
+                return obj.type == 'repeater';
+              }).each(function (obj, id) {
+                if (id == curr_repeater_id) {
+                  repeater = obj;
                 }
+              });
+              console.log(repeater.dataProps);
+              console.log(repeater.data);
 
 
-              ]
-            };
-            /* option end */
-            if (option && typeof option === "object") {
-              myChart.setOption(option, true);
+              /* get config data */
+              let configer;
+              $axure(function (obj) {
+                return obj.type == 'repeater';
+              }).each(function (obj, id) {
+                if (id == curr_config_id) {
+                  configer = obj;
+                }
+              });
+              console.log(configer.dataProps);
+              console.log(configer.data);
+
+              let xdatas = configer.data[0].xdata.text.split(',');
+
+
+
+              let dataset = [];
+              let ydatas = [];
+              for (let j = 0; j < repeater.data.length; j++) {
+
+                ydatas.push(repeater.data[j].name.text);
+                let ydata = repeater.data[j].values.text.split(',');
+
+                let datastr = {};
+                datastr.name = repeater.data[j].name.text;
+                datastr.data = repeater.data[j].values.text.split(',');
+
+                datastr.type = repeater.data[j].type.text;
+                datastr.stack = repeater.data[j].stack.text;
+                datastr.smooth = (repeater.data[j].smooth.text === 'true');
+
+                let lineStyle = {};
+                lineStyle.color = repeater.data[j].linecolor.text;
+                datastr.lineStyle = lineStyle;
+                let areaStyle = {};
+                let color = {};
+                color.type = 'linear';
+                color.x = 0;
+                color.y = 0;
+                color.x2 = 0;
+                color.y2 = 1;
+
+                let colorStops = [];
+                let colorStop = {};
+
+                colorStop.color = repeater.data[j].offset1color.text;
+                colorStop.offset = 0;
+                colorStops.push(colorStop);
+
+                colorStop.color = repeater.data[j].offset2color.text;
+                colorStop.offset = 0.5;
+                colorStops.push(colorStop);
+
+                colorStop.color = repeater.data[j].offset3color.text;
+                colorStop.offset = 1;
+                colorStops.push(colorStop);
+
+                color.colorStops = colorStops;
+                areaStyle.color = color;
+                areaStyle.opacity = repeater.data[j].opacity.text;
+
+                datastr.areaStyle = areaStyle;
+
+                console.log("datastr=", datastr);
+
+                dataset.push(datastr);
+
+              }
+
+              console.log("dataset=", dataset);
+              let myChart = echarts.init(curr_chart_dom, '[[ZRP_DISPLAYMODE]]');
+
+              option = {
+                legend: {
+                  show: (configer.data[0].legendshow.text === 'true'),
+                  top: 'top'
+                },
+                grid: {
+                  top: '10%',
+                  bottom: '10%'
+                },
+                xAxis: {
+                  type: 'category',
+                  boundaryGap: false,
+                  data: xdatas
+                },
+                yAxis: {
+                  type: 'value'
+                },
+
+                series: dataset
+              };
+
+              /* option end */
+              if (option && typeof option === "object") {
+                myChart.setOption(option, true);
+              }
             }
             break;
           default:
